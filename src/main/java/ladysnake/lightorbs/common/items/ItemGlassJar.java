04a3980ab7391ae3e630c1ad223b040cf1429180
River Leviathan <riverleviathan@gmail.com>
2018-05-02 15:20:27 +0200

    Wtf glass jar

--- src/main/java/ladysnake/lightorbs/common/items/ItemGlassJar.java
+++ src/main/java/ladysnake/lightorbs/common/items/ItemGlassJar.java
@@ -11,8 +11,12 @@ import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.Mod;
 
 public class ItemGlassJar extends Item {
     private String content;
@@ -27,52 +31,55 @@ public class ItemGlassJar extends Item {
     }
 
     @Override
+    public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        // if light orb inside, releasing it
+        if (!this.content.equals("")) {
+            if (!player.isCreative()) {
+                player.getHeldItem(hand).shrink(1);
+                if (!player.addItemStackToInventory(new ItemStack(ModItems.GLASS_JAR))) player.dropItem(new ItemStack(ModItems.GLASS_JAR), true);
+            }
+
+            if (!worldIn.isRemote) {
+                switch (this.content) {
+                    case "firefly":
+                        worldIn.spawnEntity(new EntityFirefly(worldIn, pos.getX()+hitX, pos.getY()+hitY, pos.getZ()+hitZ));
+                        break;
+                    case "psi_firefly":
+                        worldIn.spawnEntity(new EntityPsiFirefly(worldIn, pos.getX()+hitX, pos.getY()+hitY, pos.getZ()+hitZ));
+                        break;
+                    case "lightning_bug":
+                        worldIn.spawnEntity(new EntityLightningBug(worldIn, pos.getX()+hitX, pos.getY()+hitY, pos.getZ()+hitZ));
+                        break;
+                }
+            }
+        }
+        return super.onItemUse(player, worldIn, pos, hand, facing, hitX, hitY, hitZ);
+    }
+
+    @Override
     public boolean itemInteractionForEntity(ItemStack stack, EntityPlayer playerIn, EntityLivingBase target, EnumHand hand) {
         // if empty jar and entity is a firefly, catching it
         if (content.equals("") && target instanceof EntityFirefly) {
-            if (!playerIn.isCreative()) {
-                switch (EntityList.getKey(target.getClass()).toString()) {
-                    case "lightorbs:firefly":
-                        playerIn.addItemStackToInventory(new ItemStack(ModItems.FIREFLY_IN_A_JAR));
-                        break;
-                    case "lightorbs:psi_firefly":
-                        playerIn.addItemStackToInventory(new ItemStack(ModItems.PSI_FIREFLY_IN_A_JAR));
-                        break;
-                    case "lightorbs:lightning_bug":
-                        playerIn.addItemStackToInventory(new ItemStack(ModItems.LIGHTNING_BUG_IN_A_JAR));
-                        break;
-                }
+            ItemStack obtainedStack = new ItemStack(ModItems.GLASS_JAR);
+            switch (EntityList.getKey(target.getClass()).toString()) {
+                case "lightorbs:firefly":
+                    obtainedStack = new ItemStack(ModItems.FIREFLY_IN_A_JAR);
+                    break;
+                case "lightorbs:psi_firefly":
+                    obtainedStack = new ItemStack(ModItems.PSI_FIREFLY_IN_A_JAR);
+                    break;
+                case "lightorbs:lightning_bug":
+                    obtainedStack = new ItemStack(ModItems.LIGHTNING_BUG_IN_A_JAR);
+                    break;
+            }
+
+            if (!playerIn.isCreative() && !playerIn.world.isRemote) {
                 stack.shrink(1);
+                if (!playerIn.addItemStackToInventory(obtainedStack)) playerIn.dropItem(obtainedStack, true);
             }
             target.setDead();
         }
         return true;
-    }
-
-    @SuppressWarnings("NullableProblems")
-    @Override
-    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
-        // if light orb inside, releasing it
-        if (!this.content.equals("")) {
-            if (!playerIn.isCreative()) {
-                playerIn.getHeldItem(handIn).shrink(1);
-                playerIn.addItemStackToInventory(new ItemStack(ModItems.GLASS_JAR));
-            }
-            if (!worldIn.isRemote) {
-                switch (this.content) {
-                    case "firefly":
-                        worldIn.spawnEntity(new EntityFirefly(worldIn, playerIn.posX, playerIn.posY, playerIn.posZ));
-                        break;
-                    case "psi_firefly":
-                        worldIn.spawnEntity(new EntityPsiFirefly(worldIn, playerIn.posX, playerIn.posY, playerIn.posZ));
-                        break;
-                    case "lightning_bug":
-                        worldIn.spawnEntity(new EntityLightningBug(worldIn, playerIn.posX, playerIn.posY, playerIn.posZ));
-                        break;
-                }
-            }
-        }
-        return super.onItemRightClick(worldIn, playerIn, handIn);
     }
 
 }
