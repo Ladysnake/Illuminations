7e388803c74e3827809d5b0672a660fddd16410e
doctor4t <doctor4t@gmail.com>
2019-05-04 14:10:45 +0200

    Remove glowing (potion) effect from fireflies

--- src/main/java/ladysnake/illuminations/client/renders/entities/FireflyRender.java
+++ src/main/java/ladysnake/illuminations/client/renders/entities/FireflyRender.java
@@ -24,71 +24,73 @@ public class FireflyRender<T extends Entity> extends EntityRenderer<T> {
 
     @Override
     public void render(Entity entity, double x, double y, double z, float entityYaw, float partialTicks) {
-        GlStateManager.pushMatrix();
+        if (!this.field_4674) {
+            GlStateManager.pushMatrix();
 
-        GlStateManager.translatef((float) x, (float) y + 0.1f, (float) z);
+            GlStateManager.translatef((float) x, (float) y + 0.1f, (float) z);
 
-        GlStateManager.enableAlphaTest();
-        GlStateManager.enableBlend();
-        GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
-        GlStateManager.disableLighting();
+            GlStateManager.enableAlphaTest();
+            GlStateManager.enableBlend();
+            GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+            GlStateManager.disableLighting();
 
-        GLX.glMultiTexCoord2f(GLX.GL_TEXTURE1, 240f, 240f);
+            GLX.glMultiTexCoord2f(GLX.GL_TEXTURE1, 240f, 240f);
 
-        GlStateManager.rotatef(180.0F - this.renderManager.cameraYaw, 0.0F, 1.0F, 0.0F);
-        GlStateManager.rotatef((float) (this.renderManager.gameOptions.perspective == 2 ? -1 : 1) * -this.renderManager.cameraPitch, 1.0F, 0.0F, 0.0F);
+            GlStateManager.rotatef(180.0F - this.renderManager.cameraYaw, 0.0F, 1.0F, 0.0F);
+            GlStateManager.rotatef((float) (this.renderManager.gameOptions.perspective == 2 ? -1 : 1) * -this.renderManager.cameraPitch, 1.0F, 0.0F, 0.0F);
 
-        this.bindEntityTexture((T) entity);
-        if (entity instanceof FireflyEntity) {
-            float alpha = ((FireflyEntity) entity).getAlpha();
-            float scale = ((FireflyEntity) entity).getScaleModifier();
-            float color = ((FireflyEntity) entity).getColorModifier();
-            Float nextAlphaGoal = ((FireflyEntity) entity).getNextAlphaGoal();
+            this.bindEntityTexture((T) entity);
+            if (entity instanceof FireflyEntity) {
+                float alpha = ((FireflyEntity) entity).getAlpha();
+                float scale = ((FireflyEntity) entity).getScaleModifier();
+                float color = ((FireflyEntity) entity).getColorModifier();
+                Float nextAlphaGoal = ((FireflyEntity) entity).getNextAlphaGoal();
 
-            if (nextAlphaGoal == null || nextAlphaGoal.equals(round(alpha, 1))) {
-                ((FireflyEntity) entity).setNextAlphaGoal(new Random().nextInt(11) / 10.0F);
-            } else {
-                if (nextAlphaGoal > alpha) {
-                    alpha += 0.05F;
-                } else if (nextAlphaGoal < alpha) {
-                    alpha -= 0.05F;
+                if (nextAlphaGoal == null || nextAlphaGoal.equals(round(alpha, 1))) {
+                    ((FireflyEntity) entity).setNextAlphaGoal(new Random().nextInt(11) / 10.0F);
+                } else {
+                    if (nextAlphaGoal > alpha) {
+                        alpha += 0.05F;
+                    } else if (nextAlphaGoal < alpha) {
+                        alpha -= 0.05F;
+                    }
                 }
+
+                ((FireflyEntity) entity).setAlpha(Math.min(Math.max(alpha, 0), 1));
+                GlStateManager.scalef(scale, scale, scale);
+                GlStateManager.color4f(color, 1F, 0F, ((FireflyEntity) entity).getAlpha());
             }
 
-            ((FireflyEntity) entity).setAlpha(Math.min(Math.max(alpha, 0), 1));
-            GlStateManager.scalef(scale, scale, scale);
-            GlStateManager.color4f(color, 1F, 0F, ((FireflyEntity) entity).getAlpha());
+            Tessellator tessellator = Tessellator.getInstance();
+            BufferBuilder bufferbuilder = tessellator.getBufferBuilder();
+            float minU = 0;
+            float minV = 0;
+            float maxU = 1;
+            float maxV = 1;
+            bufferbuilder.begin(GL11.GL_QUADS, VertexFormats.POSITION_UV_NORMAL);
+            bufferbuilder.vertex(-0.5D, -0.25D, 0.0D).texture((double) maxU, (double) maxV).normal(0.0F, 1.0F, 0.0F).next();
+            bufferbuilder.vertex(0.5D, -0.25D, 0.0D).texture((double) minU, (double) maxV).normal(0.0F, 1.0F, 0.0F).next();
+            bufferbuilder.vertex(0.5D, 0.75D, 0.0D).texture((double) minU, (double) minV).normal(0.0F, 1.0F, 0.0F).next();
+            bufferbuilder.vertex(-0.5D, 0.75D, 0.0D).texture((double) maxU, (double) minV).normal(0.0F, 1.0F, 0.0F).next();
+            tessellator.draw();
+
+            this.bindTexture(new Identifier(Illuminations.MOD_ID, "textures/entity/firefly_overlay.png"));
+            //noinspection ConstantConditions
+            GlStateManager.color4f(1F, 1F, 1F, ((FireflyEntity) entity).getAlpha());
+            bufferbuilder.begin(7, VertexFormats.POSITION_UV_NORMAL);
+            bufferbuilder.vertex(-0.5D, -0.25D, 0.0D).texture((double) maxU, (double) maxV).normal(0.0F, 1.0F, 0.0F).next();
+            bufferbuilder.vertex(0.5D, -0.25D, 0.0D).texture((double) minU, (double) maxV).normal(0.0F, 1.0F, 0.0F).next();
+            bufferbuilder.vertex(0.5D, 0.75D, 0.0D).texture((double) minU, (double) minV).normal(0.0F, 1.0F, 0.0F).next();
+            bufferbuilder.vertex(-0.5D, 0.75D, 0.0D).texture((double) maxU, (double) minV).normal(0.0F, 1.0F, 0.0F).next();
+            tessellator.draw();
+
+            GlStateManager.disableAlphaTest();
+            GlStateManager.disableBlend();
+            GlStateManager.disableRescaleNormal();
+            GlStateManager.enableLighting();
+            GlStateManager.popMatrix();
+            super.render((T) entity, x, y, z, entityYaw, partialTicks);
         }
-
-        Tessellator tessellator = Tessellator.getInstance();
-        BufferBuilder bufferbuilder = tessellator.getBufferBuilder();
-        float minU = 0;
-        float minV = 0;
-        float maxU = 1;
-        float maxV = 1;
-        bufferbuilder.begin(GL11.GL_QUADS, VertexFormats.POSITION_UV_NORMAL);
-        bufferbuilder.vertex(-0.5D, -0.25D, 0.0D).texture((double) maxU, (double) maxV).normal(0.0F, 1.0F, 0.0F).next();
-        bufferbuilder.vertex(0.5D, -0.25D, 0.0D).texture((double) minU, (double) maxV).normal(0.0F, 1.0F, 0.0F).next();
-        bufferbuilder.vertex(0.5D, 0.75D, 0.0D).texture((double) minU, (double) minV).normal(0.0F, 1.0F, 0.0F).next();
-        bufferbuilder.vertex(-0.5D, 0.75D, 0.0D).texture((double) maxU, (double) minV).normal(0.0F, 1.0F, 0.0F).next();
-        tessellator.draw();
-
-        this.bindTexture(new Identifier(Illuminations.MOD_ID, "textures/entity/firefly_overlay.png"));
-        //noinspection ConstantConditions
-        GlStateManager.color4f(1F, 1F, 1F, ((FireflyEntity) entity).getAlpha());
-        bufferbuilder.begin(7, VertexFormats.POSITION_UV_NORMAL);
-        bufferbuilder.vertex(-0.5D, -0.25D, 0.0D).texture((double) maxU, (double) maxV).normal(0.0F, 1.0F, 0.0F).next();
-        bufferbuilder.vertex(0.5D, -0.25D, 0.0D).texture((double) minU, (double) maxV).normal(0.0F, 1.0F, 0.0F).next();
-        bufferbuilder.vertex(0.5D, 0.75D, 0.0D).texture((double) minU, (double) minV).normal(0.0F, 1.0F, 0.0F).next();
-        bufferbuilder.vertex(-0.5D, 0.75D, 0.0D).texture((double) maxU, (double) minV).normal(0.0F, 1.0F, 0.0F).next();
-        tessellator.draw();
-
-        GlStateManager.disableAlphaTest();
-        GlStateManager.disableBlend();
-        GlStateManager.disableRescaleNormal();
-        GlStateManager.enableLighting();
-        GlStateManager.popMatrix();
-        super.render((T) entity, x, y, z, entityYaw, partialTicks);
     }
 
 //    @Override
