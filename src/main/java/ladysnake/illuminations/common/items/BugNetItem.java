6f11a37b5318d0dff0b44674a557630862c26448
doctor4t <doctor4t@gmail.com>
2019-05-16 19:40:04 +0200

    Add firefly catching

--- src/main/java/ladysnake/illuminations/common/items/BugNetItem.java
+++ src/main/java/ladysnake/illuminations/common/items/BugNetItem.java
@@ -1,10 +1,13 @@
 package ladysnake.illuminations.common.items;
 
+import ladysnake.illuminations.common.Illuminations;
 import ladysnake.illuminations.common.entities.FireflyEntity;
+import ladysnake.illuminations.common.init.IlluminationsItems;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
@@ -24,8 +27,11 @@ public class BugNetItem extends Item {
             targetEntity.y = 500;
             targetEntity.z = player.z;
             targetEntity.kill();
-            
-            // TODO: Add Firefly Item
+
+            if (player instanceof PlayerEntity) {
+                player.playSound(SoundEvents.ENTITY_PLAYER_ATTACK_SWEEP, 1.0F, 1.0F);
+                ((PlayerEntity) player).inventory.insertStack(new ItemStack(IlluminationsItems.FIREFLY));
+            }
         }
 
         return false;
