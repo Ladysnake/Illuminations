67b4a0f637e91f5e37d015061e17d562de2aa299
doctor4t <doctor4t@gmail.com>
2019-05-16 19:35:11 +0200

    Add firefly items

--- src/main/java/ladysnake/illuminations/common/init/IlluminationsItems.java
+++ src/main/java/ladysnake/illuminations/common/init/IlluminationsItems.java
@@ -2,6 +2,7 @@ package ladysnake.illuminations.common.init;
 
 import ladysnake.illuminations.common.Illuminations;
 import ladysnake.illuminations.common.items.BugNetItem;
+import ladysnake.illuminations.common.items.FireflyItem;
 import ladysnake.illuminations.common.items.WillOWispItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -11,10 +12,12 @@ public class IlluminationsItems {
 
     public static Item WILL_O_WISP;
     public static Item BUG_NET;
+    public static Item FIREFLY;
 
     public static void init() {
         WILL_O_WISP = registerItem(new WillOWispItem((new Item.Settings()).itemGroup(ItemGroup.MISC)), "will_o_wisp");
         BUG_NET = registerItem(new BugNetItem((new Item.Settings()).itemGroup(ItemGroup.TOOLS)), "bug_net");
+        FIREFLY = registerItem(new FireflyItem((new Item.Settings()).itemGroup(ItemGroup.MISC)), "firefly");
     }
 
     public static Item registerItem(Item item, String name) {
---
+++ src/main/java/ladysnake/illuminations/common/items/FireflyItem.java
@@ -0,0 +1,26 @@
+package ladysnake.illuminations.common.items;
+
+import ladysnake.illuminations.common.entities.FireflyEntity;
+import ladysnake.illuminations.common.init.IlluminationsEntities;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+
+public class FireflyItem extends Item {
+    public FireflyItem(Settings item$Settings_1) {
+        super(item$Settings_1);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
+        world.spawnEntity(new FireflyEntity(world, playerEntity.x, playerEntity.y, playerEntity.z));
+        if (!playerEntity.isCreative()) {
+            playerEntity.getStackInHand(hand).subtractAmount(1);
+        }
+        return new TypedActionResult(ActionResult.SUCCESS, playerEntity.getStackInHand(hand));
+    }
+}
