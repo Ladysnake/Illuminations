c24d4672533eaa23ff1c3ff2fe6eb763d69c4cff
doctor4t <doctor4t@gmail.com>
2019-12-22 20:20:02 +0100

    Remove bug net item

--- src/main/java/ladysnake/illuminations/common/init/IlluminationsItems.java
+++ src/main/java/ladysnake/illuminations/common/init/IlluminationsItems.java
@@ -1,7 +1,6 @@
 package ladysnake.illuminations.common.init;
 
 import ladysnake.illuminations.common.Illuminations;
-import ladysnake.illuminations.common.items.BugNetItem;
 import ladysnake.illuminations.common.items.FireflyItem;
 import ladysnake.illuminations.common.items.GlowMealItem;
 import net.minecraft.item.Item;
@@ -10,14 +9,12 @@ import net.minecraft.util.registry.Registry;
 
 public class IlluminationsItems {
 
-    public static Item BUG_NET;
     public static Item FIREFLY;
     public static Item GLOW_MEAL;
 
     public static void init() {
-        BUG_NET = registerItem(new BugNetItem((new Item.Settings()).maxDamage(238).group(ItemGroup.TOOLS)), "bug_net");
-        FIREFLY = registerItem(new FireflyItem((new Item.Settings()).group(ItemGroup.MISC)), "firefly");
-        GLOW_MEAL = registerItem(new GlowMealItem((new Item.Settings()).group(ItemGroup.MISC)), "glow_meal");
+        FIREFLY = registerItem(new FireflyItem((new Item.Settings()).itemGroup(ItemGroup.MISC)), "firefly");
+        GLOW_MEAL = registerItem(new GlowMealItem((new Item.Settings()).itemGroup(ItemGroup.MISC)), "glow_meal");
     }
 
     public static Item registerItem(Item item, String name) {
