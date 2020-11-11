51a18e49d6763d1fe5d489e639fd15ec8d84d73b
doctor4t <25477005+doctor4t@users.noreply.github.com>
2020-11-11 17:46:47 +0100

    Add "update available" toast

---
+++ src/main/java/ladysnake/illuminations/client/UpdateToast.java
@@ -0,0 +1,44 @@
+package ladysnake.illuminations.client;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.toast.Toast;
+import net.minecraft.client.toast.ToastManager;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.LiteralText;
+
+public class UpdateToast implements Toast {
+    @Override
+    public Visibility draw(MatrixStack matrices, ToastManager manager, long startTime) {
+        manager.getGame().getTextureManager().bindTexture(Toast.TEXTURE);
+        RenderSystem.color3f(1.0F, 1.0F, 1.0F);
+        manager.drawTexture(matrices, 0, 0, 0, 64, 160, 32);
+        manager.getGame().textRenderer.draw(matrices, new LiteralText("Illuminations"), 18, 7, -25);
+        manager.getGame().textRenderer.draw(matrices, new LiteralText("Update motherfucker do you want it?"), 18, 18, -1);
+        return MinecraftClient.getInstance().currentScreen instanceof TitleScreen ? Toast.Visibility.SHOW : Toast.Visibility.HIDE;
+    }
+
+    @Override
+    public Object getType() {
+        return TYPE;
+    }
+
+    @Override
+    public int getWidth() {
+        return 160;
+    }
+
+    @Override
+    public int getHeight() {
+        return 32;
+    }
+
+    public static void add() {
+        ToastManager toastManager = MinecraftClient.getInstance().getToastManager();
+        UpdateToast toast = toastManager.getToast(UpdateToast.class, Toast.TYPE);
+        if (toast == null) {
+            toastManager.add(new UpdateToast());
+        }
+    }
+}
