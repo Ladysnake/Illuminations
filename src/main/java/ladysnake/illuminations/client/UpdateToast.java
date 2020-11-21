b952057da472b0e42167060e867ae9a47194a28c
doctor4t <25477005+doctor4t@users.noreply.github.com>
2020-11-21 15:29:47 +0100

    Finalize update toast

--- src/main/java/ladysnake/illuminations/client/UpdateToast.java
+++ src/main/java/ladysnake/illuminations/client/UpdateToast.java
@@ -14,9 +14,10 @@ public class UpdateToast implements Toast {
     public Visibility draw(MatrixStack matrices, ToastManager manager, long startTime) {
         manager.getGame().getTextureManager().bindTexture(new Identifier("illuminations", "textures/gui/update_toast.png"));
         RenderSystem.color3f(1.0F, 1.0F, 1.0F);
-        manager.drawTexture(matrices, 0, 0, 0, 64, 160, 32);
-        manager.getGame().textRenderer.draw(matrices, new LiteralText("Illuminations"), 18, 7, -256);
-        manager.getGame().textRenderer.draw(matrices, new LiteralText("Update available!"), 18, 18, -1);
+        manager.drawTexture(matrices, 0, 0, 0, 0, getWidth(), getHeight());
+        manager.getGame().textRenderer.draw(matrices, new LiteralText("Illuminations update available!"), 32, 7, -256);
+        manager.getGame().textRenderer.draw(matrices, new LiteralText("Update automatically downloaded"), 32, 18, -1);
+        manager.getGame().textRenderer.draw(matrices, new LiteralText("Restart your game to install it"), 32, 29, -1);
         return MinecraftClient.getInstance().currentScreen instanceof TitleScreen ? Toast.Visibility.SHOW : Toast.Visibility.HIDE;
     }
 
@@ -27,12 +28,12 @@ public class UpdateToast implements Toast {
 
     @Override
     public int getWidth() {
-        return 160;
+        return 200;
     }
 
     @Override
     public int getHeight() {
-        return 32;
+        return 43;
     }
 
     public static void add() {
