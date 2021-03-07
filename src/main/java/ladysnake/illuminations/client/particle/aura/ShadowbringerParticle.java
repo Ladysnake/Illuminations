7bbf0d12dd9082ef4855c2698d1d9275c263bcf3
Dragonoidzero <danielferrerreyes@protonmail.com>
2021-03-07 02:20:05 -0600

    Shadowbringer aura complete

--- src/main/java/ladysnake/illuminations/client/particle/aura/ShadowbringerParticle.java
+++ src/main/java/ladysnake/illuminations/client/particle/aura/ShadowbringerParticle.java
@@ -15,19 +15,24 @@ import net.minecraft.util.math.BlockPos;
 public class ShadowbringerParticle extends ChorusPetalParticle {
 
     boolean negateX = random.nextBoolean(), negateZ = random.nextBoolean();
+    private final SpriteProvider spriteProvider;
+    private final float randEffect = random.nextFloat() + 0.5F;
+    int spriteState = 0;
 
     public ShadowbringerParticle(ClientWorld world, double x, double y, double z, double velocityX, double velocityY, double velocityZ, SpriteProvider spriteProvider) {
         super(world, x, y, z, velocityX, velocityY, velocityZ, spriteProvider);
 
-        this.maxAge = 60 + random.nextInt(60);
-        this.velocityY = random.nextFloat()/10;
-        this.velocityX = negateX ? -random.nextGaussian()/70 : random.nextGaussian()/70;
-        this.velocityZ = negateZ ? -random.nextGaussian()/70 : random.nextGaussian()/70;
+        this.maxAge = 100 + random.nextInt(40);
+        this.velocityY = (0.2 + random.nextFloat()) / 10;
+        this.velocityX = negateX ? -random.nextGaussian()/50 : random.nextGaussian()/50;
+        this.velocityZ = negateZ ? -random.nextGaussian()/50 : random.nextGaussian()/50;
+        this.spriteProvider = spriteProvider;
 
+        this.setSprite(spriteProvider.getSprite(0, 3));
         colorAlpha = 0;
 
 
-        this.setPos(this.x + TwilightFireflyParticle.getWanderingDistance(this.random), this.y, this.z + TwilightFireflyParticle.getWanderingDistance(this.random));
+        this.setPos(this.x + TwilightFireflyParticle.getWanderingDistance(this.random), this.y + random.nextFloat() * 1.5, this.z + TwilightFireflyParticle.getWanderingDistance(this.random));
     }
 
     @Environment(EnvType.CLIENT)
@@ -45,20 +50,25 @@ public class ShadowbringerParticle extends ChorusPetalParticle {
 
     public void tick() {
         if (this.age++ < this.maxAge) {
-            this.colorAlpha = Math.min(1f, this.colorAlpha + 0.02f);
+            this.colorAlpha = Math.min(1f, this.colorAlpha + 0.045f);
         }
 
         this.prevPosX = this.x;
         this.prevPosY = this.y;
         this.prevPosZ = this.z;
 
-        velocityX = velocityX * 0.7 + (negateX ? -(Math.sin(age / 3.0) / 20.0) : Math.sin(age / 3.0) / 20.0);
-        velocityZ = velocityZ * 0.7 + (negateZ ? -(Math.sin(age / 3.0) / 20.0) : Math.sin(age / 3.0) / 20.0);
+        velocityX = velocityX * 0.85 + (negateX ? -(Math.sin(age / (2.0 + randEffect)) / 20.0) : Math.sin(age / 3.0) / 20.0);
+        velocityZ = velocityZ * 0.85 + (negateZ ? -(Math.sin(age / (2.0 + randEffect)) / 20.0) : Math.sin(age / 3.0) / 20.0);
 
         this.move(this.velocityX, this.velocityY, this.velocityZ);
 
+        if(age > 0 && maxAge > 0) {
+            float agePercent = (float) ((float) age / maxAge);
+            this.setSprite(spriteProvider.getSprite(Math.min(3, (int) (agePercent * 4)), 3));
+        }
+
         if (this.age >= this.maxAge) {
-            this.colorAlpha = Math.max(0f, this.colorAlpha-0.01f);
+            this.colorAlpha = Math.max(0f, this.colorAlpha-0.015f);
 
             if (this.colorAlpha <= 0f) {
                 this.markDead();
