953bf005b16a523dd7ad5c9bb44a789aa883b535
Joththeboop <64709045+Joththeboop@users.noreply.github.com>
2021-03-29 15:30:35 +0300

    Update IlluminationsClient.java

--- src/main/java/ladysnake/illuminations/client/IlluminationsClient.java
+++ src/main/java/ladysnake/illuminations/client/IlluminationsClient.java
@@ -95,6 +95,11 @@ public class IlluminationsClient implements ClientModInitializer {
     public static DefaultParticleType PRIDE_OVERHEAD;
     public static DefaultParticleType TRANS_PRIDE_OVERHEAD;
     public static DefaultParticleType JACKO_OVERHEAD;
+    public static DefaultParticleType LESBIAN_PRIDE_OVERHEAD;
+    public static DefaultParticleType BI_PRIDE_OVERHEAD;
+    public static DefaultParticleType ACE_PRIDE_OVERHEAD;
+    public static DefaultParticleType NB_PRIDE_OVERHEAD;
+    public static DefaultParticleType INTERSEX_PRIDE_OVERHEAD;
 
     // spawn biomes
     public static ImmutableMap<Biome.Category, ImmutableSet<IlluminationData>> ILLUMINATIONS_BIOME_CATEGORIES;
@@ -155,16 +160,16 @@ public class IlluminationsClient implements ClientModInitializer {
         ParticleFactoryRegistry.getInstance().register(IlluminationsClient.TRANS_PRIDE_OVERHEAD, OverheadParticle.DefaultFactory::new);
         JACKO_OVERHEAD = Registry.register(Registry.PARTICLE_TYPE, "illuminations:jacko_overhead", FabricParticleTypes.simple(true));
         ParticleFactoryRegistry.getInstance().register(IlluminationsClient.JACKO_OVERHEAD, JackoParticle.DefaultFactory::new);
-        LESBIAN = Registry.register(Registry.PARTICLE_TYPE, "illuminations:lesbian", FabricParticleTypes.simple(true));
-        ParticleFactoryRegistry.getInstance().register(IlluminationsClient.LESBIAN, OverheadParticle.DefaultFactory::new);
-        BI = Registry.register(Registry.PARTICLE_TYPE, "illuminations:bi", FabricParticleTypes.simple(true));
-        ParticleFactoryRegistry.getInstance().register(IlluminationsClient.BI, OverheadParticle.DefaultFactory::new);
-        ACE = Registry.register(Registry.PARTICLE_TYPE, "illuminations:ACE", FabricParticleTypes.simple(true));
-        ParticleFactoryRegistry.getInstance().register(IlluminationsClient.ACE, OverheadParticle.DefaultFactory::new);
-        NB = Registry.register(Registry.PARTICLE_TYPE, "illuminations:NB", FabricParticleTypes.simple(true));
-        ParticleFactoryRegistry.getInstance().register(IlluminationsClient.NB, OverheadParticle.DefaultFactory::new);
-        INTERSEX = Registry.register(Registry.PARTICLE_TYPE, "illuminations:intersex", FabricParticleTypes.simple(true));
-        ParticleFactoryRegistry.getInstance().register(IlluminationsClient.intersex, OverheadParticle.DefaultFactory::new);  ///haha sex
+        LESBIAN_PRIDE_OVERHEAD = Registry.register(Registry.PARTICLE_TYPE, "illuminations:lesbian_pride_overhead", FabricParticleTypes.simple(true));
+        ParticleFactoryRegistry.getInstance().register(IlluminationsClient.LESBIAN_PRIDE_OVERHEAD, OverheadParticle.DefaultFactory::new);
+        BI_PRIDE_OVERHEAD = Registry.register(Registry.PARTICLE_TYPE, "illuminations:bi_pride_overhead", FabricParticleTypes.simple(true));
+        ParticleFactoryRegistry.getInstance().register(IlluminationsClient.BI_PRIDE_OVERHEAD, OverheadParticle.DefaultFactory::new);
+        ACE_PRIDE_OVERHEAD = Registry.register(Registry.PARTICLE_TYPE, "illuminations:ace_pride_overhead", FabricParticleTypes.simple(true));
+        ParticleFactoryRegistry.getInstance().register(IlluminationsClient.ACE_PRIDE_OVERHEAD, OverheadParticle.DefaultFactory::new);
+        NB_PRIDE_OVERHEAD = Registry.register(Registry.PARTICLE_TYPE, "illuminations:nb_pride_overhead", FabricParticleTypes.simple(true));
+        ParticleFactoryRegistry.getInstance().register(IlluminationsClient.NB_PRIDE_OVERHEAD, OverheadParticle.DefaultFactory::new);
+        INTERSEX_PRIDE_OVERHEAD = Registry.register(Registry.PARTICLE_TYPE, "illuminations:intersex_pride_overhead", FabricParticleTypes.simple(true));
+        ParticleFactoryRegistry.getInstance().register(IlluminationsClient.INTERSEX_PRIDE_OVERHEAD, OverheadParticle.DefaultFactory::new);  ///haha sex
 
         // crowns feature
         LivingEntityFeatureRendererRegistrationCallback.EVENT.register((entityType, livingEntityRenderer, registrationHelper) -> {
@@ -214,11 +219,11 @@ public class IlluminationsClient implements ClientModInitializer {
                 .put("pride", PRIDE_OVERHEAD)
                 .put("trans_pride", TRANS_PRIDE_OVERHEAD)
                 .put("jacko", JACKO_OVERHEAD)
-                .put("lesbian", LESBIAN)
-                .put("bi", BI)
-                .put("ace", ACE)
-                .put("nb", NB)
-                .put("intersex", INTERSEX)
+                .put("lesbian_pride_overhead", LESBIAN_PRIDE_OVERHEAD)
+                .put("bi_pride_overhead", BI_PRIDE_OVERHEAD)
+                .put("ace_pride_overhead", ACE_PRIDE_OVERHEAD)
+                .put("nb_pride_overhead", NB_PRIDE_OVERHEAD)
+                .put("intersex_pride_overhead", INTERSEX_PRIDE_OVERHEAD)
                 .build();
         OVERHEADS_DATA = ImmutableMap.<String, OverheadData>builder()
                 .put("solar_crown", new OverheadData(new CrownEntityModel(), "solar_crown"))
